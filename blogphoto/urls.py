aa61a1c8bb114254ac6cc4503e505163ac0d33e9
cubouyaka <victor.fontaine95@gmail.com>
2023-10-07 11:58:27 +0200

    Factoristaion of the view login into a class

--- blogphoto/urls.py
+++ blogphoto/urls.py
@@ -21,7 +21,7 @@ import authentication.views, blog.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', authentication.views.login_page, name='login'),
+    path('', authentication.views.LoginPageView.as_view(), name='login'),
     path('logout/', authentication.views.logout_page, name='logout'),
     path('home/', blog.views.home, name='home'),
 ]
