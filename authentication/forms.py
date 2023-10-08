6b4afb267be1a70be696bfbca8fa08c48a1c3dd1
cubouyaka <victor.fontaine95@gmail.com>
2023-10-08 17:44:15 +0200

    Creation of a Login page with a LoginView, and a sign up page with a form  inheriting from UserCreationForm.

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,5 +1,7 @@
-from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=70, label='Username')
-    password = forms.CharField(max_length=70, widget=forms.PasswordInput, label='Password')
+class SignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name', 'role']
